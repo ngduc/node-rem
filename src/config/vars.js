2b1bc9435ca653d78375129eb2ae2f71f337c641
ngduc <ducjava@gmail.com>
2018-09-26 07:26:24 -0700

    fix: put back .env.example for dotenv-safe

--- src/config/vars.js
+++ src/config/vars.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 // import .env variables
 require('dotenv-safe').load({
-  path: path.join(__dirname, '../../.env')
+  path: path.join(__dirname, '../../.env'),
+  sample: path.join(__dirname, '../../.env.example')
 });
 
 module.exports = {
