535f2ea68e2efb52d0e5867f2cd23c38269de30d
ngduc <ducjava@gmail.com>
2018-09-26 01:48:10 -0700

    fix: vars.js: removed .env.example

--- src/config/vars.js
+++ src/config/vars.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 // import .env variables
 require('dotenv-safe').load({
-  path: path.join(__dirname, '../../.env'),
-  sample: path.join(__dirname, '../../.env.example'),
+  path: path.join(__dirname, '../../.env')
 });
 
 module.exports = {
@@ -12,9 +11,7 @@ module.exports = {
   jwtSecret: process.env.JWT_SECRET,
   jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
   mongo: {
-    uri: process.env.NODE_ENV === 'test'
-      ? process.env.MONGO_URI_TESTS
-      : process.env.MONGO_URI,
+    uri: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TESTS : process.env.MONGO_URI
   },
-  logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
+  logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev'
 };
